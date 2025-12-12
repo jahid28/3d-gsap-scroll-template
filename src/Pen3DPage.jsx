5165478c3d8df0a5eedf14f487bf01d04814d8d5
Rohit <rkurup01@gmail.com>
2025-12-13 07:05:18 +0800

    updated glass look for pen3d page and yyss page

--- src/Pen3DPage.jsx
+++ src/Pen3DPage.jsx
@@ -10,18 +10,44 @@ export default function Pen3DPage() {
     '/images/3dpen_12.jpg',
   ];
 
+  const GlassBox = ({ children, color = '#ffffff', blur = 7,intensity=0.15, padding='0px', ...props }) => {
+  const hexToRgba = (hex, alpha) => {
+    const r = parseInt(hex.slice(1, 3), 16);
+    const g = parseInt(hex.slice(3, 5), 16);
+    const b = parseInt(hex.slice(5, 7), 16);
+    return `rgba(${r}, ${g}, ${b}, ${alpha})`;
+  };
+
+  const glassStyle = {  
+    backgroundColor: `${hexToRgba(color,intensity)}`,
+    backdropFilter: `blur(7px)`,
+    WebkitBackdropFilter: `blur(${blur}px)`, // for safari support
+    boxShadow: '10px 10px 10px rgba(30, 30, 30, 0.4)',
+    borderLeft: `solid 1px rgba(255,255,255,0.03)}`,
+    borderTop: `solid 1px rgba(255,255,255,0.03)}`,
+    borderRadius: '16px',
+    padding: padding,
+    ...props.style
+  };
+
+  return <div style={glassStyle} {...props} className='mt-8 '>{children}</div>;
+};
+
   return (
-    <div className="bg-[#1a1a1a] text-white px-6 py-16 space-y-16">
+    <div className="bg-gradient-to-t from-black from-10% to-red-500  text-white px-6 py-16 space-y-16">
       <div className="max-w-6xl mx-auto pt-12">
       <div className="flex items-start justify-between w-full">
       <p className="text-white text-lg">June 2025</p>
-          <h1 className="text-5xl text-orange-600 font-bold text-right leading-[0.9]">3D Pen Series<br />Workshop</h1>
+          <h1 className="text-5xl text-orange-600 font-bold text-right leading-[0.9] bg-gradient-to-r from-red-400 to-white inline-block text-transparent bg-clip-text pb-2">3D Pen Series<br />Workshop</h1>
         </div>
       </div>
 
         {/* Showcase */}
+        <GlassBox color="#ffffff" blur={10} padding='30px'>
         <div className="text-white mt-16">
-          <h2 className="text-center text-4xl font-bold mb-4">Design Showcase</h2>
+          <div className='text-center'>
+            <h2 className="bg-gradient-to-r from-red-400 to-white inline-block text-transparent bg-clip-text text-4xl font-bold mb-4 pb-1">Design Showcase</h2>
+          </div>
           <p className="text-center mb-8">Our participants did an awesome job!</p>
           <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8 justify-center">
             {showcaseImages.map((src, i) => (
@@ -42,47 +68,65 @@ export default function Pen3DPage() {
             ))}
           </div>
         </div>
+        </GlassBox>
 
       {/* Section 1 */}
+        <GlassBox color="#ffffff" blur={10}>
         <div className="max-w-screen-xl mx-auto flex flex-col md:flex-row items-center justify-center gap-8">
           <div className="flex-1 flex justify-center items-center py-8 max-w-xl w-full">
             <img
               src="/images/3dpen_1.jpg"
               alt="Picture"
-              className="rounded-xl w-full h-auto max-h-[600px] object-cover"
+              className="rounded-xl w-full h-auto max-h-[600px] object-fit"
             />
           </div>
 
           <div className="flex-1 flex flex-col justify-center items-start py-8">
-            <h3 className="text-2xl font-extrabold pb-8">Unleashing Creativity with Technology</h3>
+            <h3 className="text-2xl font-extrabold pb-8 bg-gradient-to-r from-red-400 to-white inline-block text-transparent bg-clip-text text-4xl font-bold mb-4">Unleashing Creativity with Technology</h3>
+            <GlassBox color="#ffffff" blur={10} padding='16px'>
             <p>On 5 June, Mecatron proudly conducted a 3D Pen Workshop for a vibrant group of 38 participants from Vietnam. As part of our growing 3D Pen Series, this hands-on session aimed to blend creativity and technology, allowing participants to explore 3D art using specially provided pens.</p>
+            </GlassBox>
           </div>
         </div>
+        </GlassBox>
 
     {/* Section 2 */}
+    <GlassBox color="#ffffff" blur={10} padding='20px'>
     <div className="max-w-screen-xl mx-auto flex flex-col md:flex-row items-center justify-center gap-8">
     
       <div className="flex-1 flex flex-col justify-center items-start">
-            <h3 className="text-2xl font-extrabold pb-8">First-Time Wonders with 3D</h3>
+            <h3 className="text-2xl font-extrabold pb-8 bg-gradient-to-r from-red-400 to-white inline-block text-transparent bg-clip-text text-4xl font-bold mb-4 ">First-Time Wonders with 3D</h3>
+            <GlassBox color="#ffffff" blur={10} padding='16px'>
             <p>For many, it was their first time experimenting with this medium, and the results were nothing short of inspiring. The room was buzzing with energy as attendees took on the challenge of bringing their imaginations to life in 3D form.</p>
+            </GlassBox>
+
       </div>
 
       <div className="flex-1 flex justify-center items-center">
         <ImageCarousel1 />
       </div>
     </div>  
+    </GlassBox>
+
+
 
     {/* Section 3 */}
+    <GlassBox color="#ffffff" blur={10} padding='20px'>
+
     <div className="max-w-screen-xl mx-auto flex flex-col md:flex-row items-center justify-center gap-8">
 
       <div className="flex-1 flex justify-center items-center">
         <ImageCarousel2 />
       </div>
       <div className="flex-1 flex flex-col justify-center items-start">
-            <h3 className="text-2xl font-extrabold pb-8">Guided Exploration and Collaborative Learning</h3>
+            <h3 className="text-2xl font-extrabold pb-8 bg-gradient-to-r from-red-400 to-white inline-block text-transparent bg-clip-text text-4xl font-bold mb-4">Guided Exploration and Collaborative Learning</h3>
+            <GlassBox color="#ffffff" blur={10} padding='16px'>
             <p>Participants were encouraged to let their creativity flow freely, some worked entirely freehand, while others carefully followed stencils to craft detailed shapes and figures. The workshop environment fostered curiosity and patience, as individuals helped each other troubleshoot, shared ideas, and celebrated completed works together.</p>
+            </GlassBox>
       </div>
     </div>  
+      </GlassBox>
+
 
     </div>
   );
--- src/YYSSPage.jsx
+++ src/YYSSPage.jsx
@@ -1,60 +1,101 @@
 import React, { useState, useEffect } from 'react';
+import { backgroundIntensity } from 'three/src/nodes/TSL.js';
+
+const GlassBox = ({ children, color = '#ffffff', blur = 7,intensity=0.15, padding='0px', ...props }) => {
+  const hexToRgba = (hex, alpha) => {
+    const r = parseInt(hex.slice(1, 3), 16);
+    const g = parseInt(hex.slice(3, 5), 16);
+    const b = parseInt(hex.slice(5, 7), 16);
+    console.log(r,g,b);
+    return `rgba(${r}, ${g}, ${b}, ${alpha})`;
+  };
+
+  const glassStyle = {  
+    backgroundColor: `${hexToRgba(color,intensity)}`,
+    backdropFilter: `blur(${blur}px)`,
+    WebkitBackdropFilter: `blur(${blur}px)`, // for safari
+    boxShadow: '10px 10px 10px rgba(30, 30, 30, 0.4)',
+    borderLeft: `solid 1px rgba(255,255,255)}`,
+    borderTop: `solid 1px rgba(255, 255, 255)}`,
+    borderRadius: '16px',
+    padding: padding,
+    ...props.style
+  };
+
+
+  return <div style={glassStyle} {...props} className='mt-8 '>{children}</div>;
+};
 
 
 export default function YYSSPage() {
   return (
-    <div className="bg-[#1a1a1a] text-white px-6 py-16 space-y-16">
+    
+    <div className="bg-gradient-to-t from-black from-10% to-red-500 text-white px-6 py-16 space-y-16 ">
       <div className="max-w-6xl mx-auto pt-12">
         <div className="flex items-start justify-between w-full">
           <p className="text-white text-lg">June 2025</p>
-          <h1 className="text-5xl text-orange-600 font-bold text-right leading-[0.9]">
+          <h1 className="text-5xl font-bold text-right leading-[0.9] bg-gradient-to-r from-red-400 to-white inline-block text-transparent bg-clip-text p-2">
             YYSS Marine<br />Tech Workshop
           </h1>
         </div>
 
         {/* Section 1 */}
-        <div className="max-w-screen-xl mx-auto flex flex-col md:flex-row items-center justify-center gap-8">
-          {/* Photo */}
-          <div className="flex-1 flex justify-center items-center py-8 max-w-xl w-full">
-            <PolaroidPhoto
-              src="/images/yyss_1.jpg"
-              alt="YYSS Mass Photo"
-              caption="Everyone’s proud to showcase their vehicles!"
-              rotate="-rotate-6"
-              photocolour='#EA580C'
-            />
-          </div>
+        <GlassBox color="#FFFFFF" blur={10}>
+          <div className="max-w-screen-xl mx-auto flex flex-col md:flex-row items-center justify-center gap-8 p-4">
+            {/* Photo */}
+            <div className="flex-1 flex justify-center items-center py-8 max-w-xl w-full">
+              <PolaroidPhoto
+                src="/images/yyss_1.jpg"
+                alt="YYSS Mass Photo"
+                caption="Everyone’s proud to showcase their vehicles!"
+                rotate="-rotate-6"
+                photocolour='#FFFFFF'
+              />
+            </div>
 
-          {/* Text */}
-          <div className="flex-1 flex flex-col justify-center items-start py-8">
-            <h3 className="text-2xl font-extrabold pb-8">Marine Tech in Action: A 3-Day Build-and-Learn Adventure</h3>
-            <p>As part of the Yuying Secondary School (YYSS) Upper Secondary Day Camp, students took a deep dive into the world of marine engineering in our Marine Tech in Action workshop. Over three immersive days (2-4 June 2025), they explored the powerful role marine vehicles play in addressing real-world challenges, such as ocean waste retrieval, underwater inspection, and marine sampling.</p>
+            {/* Text */}
+            <div className="flex-1 flex flex-col justify-center items-start py-8">
+              <h3 className="text-2xl font-extrabold pb-8 bg-gradient-to-r from-red-400 to-white inline-block text-transparent bg-clip-text p-2">Marine Tech in Action: A 3-Day Build-and-Learn Adventure</h3>
+              <GlassBox color="#FFFFFF" blur={10} intensity={0.3} padding='16px'>
+
+              <p>As part of the Yuying Secondary School (YYSS) Upper Secondary Day Camp, students took a deep dive into the world of marine engineering in our Marine Tech in Action workshop. Over three immersive days (2-4 June 2025), they explored the powerful role marine vehicles play in addressing real-world challenges, such as ocean waste retrieval, underwater inspection, and marine sampling.</p>
+              </GlassBox>
+            </div>
           </div>
-        </div>
+        </GlassBox>
 
         {/* Section 2 */}
-        <div className="max-w-screen-xl mx-auto flex flex-col md:flex-row items-center justify-center gap-8">
+          <GlassBox color="#FFFFFF" blur={10}> 
+          <div className="max-w-screen-xl mx-auto flex flex-col md:flex-row items-center justify-center gap-8 p-4">
 
-          {/* Text */}
-          <div className="flex-1 flex flex-col justify-center items-start py-8">
-            <p>With hands-on kits developed by Mecatron, students took on the roles of engineers and assembled their own surface vehicles. The workshop culminated in a friendly poolside showdown, where teams tested their creations in a mini competition!</p>
+            {/* Text */}
+
+            <div className="flex-1 flex flex-col justify-center items-start py-8">
+            <GlassBox color="#FFFFFF" blur={10} intensity={0.3} padding='16px'>
+
+              <p>With hands-on kits developed by Mecatron, students took on the roles of engineers and assembled their own surface vehicles. The workshop culminated in a friendly poolside showdown, where teams tested their creations in a mini competition!</p>
+            </GlassBox>
+
+            </div>
+  
+
+            {/* Photo */}
+            <div className="flex-1 flex justify-center items-center py-8 max-w-xl w-full">
+              <PolaroidPhoto
+                src="/images/yyss_3.jpg"
+                alt="YYSS Photo 3"
+                caption="Teamwork makes the dream work~"
+                rotate="rotate-3"
+                photocolour='#FFFFFF'
+              />
+            </div>
+
           </div>
-
-          {/* Photo */}
-          <div className="flex-1 flex justify-center items-center py-8 max-w-xl w-full">
-            <PolaroidPhoto
-              src="/images/yyss_3.jpg"
-              alt="YYSS Photo 3"
-              caption="Teamwork makes the dream work~"
-              rotate="rotate-3"
-              photocolour='#EA580C'
-            />
-          </div>
-
-        </div>
+          </GlassBox>
 
         {/* Section 3 */}
-        <div className="max-w-screen-xl mx-auto flex flex-col md:flex-row items-center justify-center gap-8">
+        <GlassBox color="#FFFFFF" blur={10}>
+        <div className="max-w-screen-xl mx-auto flex flex-col md:flex-row items-center justify-center gap-8 p-4">
           {/* Photo */}
           <div className="flex-1 flex justify-center items-center py-8 max-w-xl w-full">
             <PolaroidPhoto
@@ -62,17 +103,24 @@ export default function YYSSPage() {
               alt="YYSS Photo 6"
               caption="Pep talk before the competition ^v^"
               rotate="-rotate-6"
-              photocolour='#EA580C'
+              photocolour='#FFFFFF'
             />
           </div>
 
           {/* Text */}
           <div className="flex-1 flex flex-col justify-center items-start py-8">
-            <h3 className="text-2xl font-extrabold pb-8">Engineering Meets Environmental Responsibility</h3>
+            <h3 className="text-2xl font-extrabold pb-8 bg-gradient-to-r from-red-400 to-white inline-block text-transparent bg-clip-text pb-2">Engineering Meets Environmental Responsibility</h3>
+            <GlassBox color="#FFFFFF" blur={10} intensity={0.3} padding='16px'>
             <p>Beyond technical skills, this workshop emphasised the real-world impact of marine engineering. Through interactive underwater simulations and design thinking exercises, students discovered how innovation in marine tech contributes to ocean sustainability and responsible design.</p>
+            </GlassBox>
           </div>
         </div>
-        <h2 className="text-4xl font-extrabold text-orange-500 mt-12">Photos & Video Montage</h2>
+        </GlassBox>
+
+        <GlassBox color="#FFFFFF" blur={10}>
+        <div className='text-center'>
+          <h2 className="text-4xl font-extrabold text-orange-500 mt-12 text- bg-gradient-to-r from-red-400 to-white inline-block text-transparent bg-clip-text pb-2">Photos & Video Montage</h2>
+        </div>
 
         <div className="max-w-screen-xl mx-auto flex flex-col md:flex-row items-start justify-center sm:px-6 gap-6 py-12 overflow-x-hidden">
 
@@ -95,7 +143,10 @@ export default function YYSSPage() {
               />
             </div>
           </div>
+
         </div>
+        </GlassBox>
+
 
       </div>
 
@@ -116,7 +167,7 @@ function PolaroidPhoto({ src, alt, caption, rotate = '-rotate-3', photocolour = 
         className="w-96 aspect-[5/3] object-cover mx-auto rounded-md"
       />
       {caption && (
-        <p className="text-white mt-2 text-sm italic font-bold">{caption}</p>
+        <p className="text-red-600 mt-2 text-sm italic font-bold">{caption}</p>
       )}
     </div>
   );
