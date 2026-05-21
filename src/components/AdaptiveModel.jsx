6b090502b03bf6c7a87f85f49b7d3a3ddc59a136
Tan Zi Xuan <tanzixuan88@gmail.com>
2026-05-21 12:02:43 +0800

    Updated AdaptiveModel

--- src/components/AdaptiveModel.jsx
+++ src/components/AdaptiveModel.jsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react';
+import { useMemo } from 'react';
 import { useGLTF } from '@react-three/drei';
 import { KevinOptimized } from '../KevinOptimized';
 import { LucyOptimized } from '../LucyOptimized';
